2eb16f89ad1d50775bf84a8773db8f22016d4870
MalluCortez <maria.santana@outlook.com.br>
2023-01-14 15:47:45 -0300

    requisito 1 feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userLogin } from '../redux/actions';
+
+const passwordNumberMin = 6;
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(userLogin(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <main>
+        <form>
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+            value={ email }
+            placeholder="Digite seu email"
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={ password }
+            placeholder="Digite sua senha"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            onClick={ this.handleSubmit }
+            disabled={ password.length < passwordNumberMin || !email.match(
+              /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm,
+            ) }
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
