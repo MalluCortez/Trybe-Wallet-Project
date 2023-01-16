8d4589a01e30e0c372ebd8e4e918d25078dd539c
MalluCortez <maria.santana@outlook.com.br>
2023-01-15 21:07:06 -0300

    requisito 6 adicionar o componente na página

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
 import fetchCurr from '../services/currAPI';
 import { getRequestCurr } from '../redux/actions/index';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   state = {
@@ -33,6 +34,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
